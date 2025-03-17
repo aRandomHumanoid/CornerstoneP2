875083c6aadb7ce3dd43cfb7d2893f70923e203a
avats123 <anshvats12345@gmail.com>
2025-03-17 18:12:28 -0400

    Update P2ProofOfConcept.ino
    bandoabioawe

--- Code/P2ProofOfConcept/P2ProofOfConcept.ino
+++ Code/P2ProofOfConcept/P2ProofOfConcept.ino
@@ -1,32 +1,34 @@
-const int numSquares = 4;
-const int pins[] = {1, 2, 3, 4};
+const int numSquares = 4; //total num of squares;
+const int pins[] = {1, 2, 3, 4}; 
 
 const int numBuildings = 5;
 const int buildingVoltages[] = {10, 20, 30, 40, 50};
 
-const int tolerance = 5;
+const int tolerance = 5; //tolerance for voltage
 
-int boardState[numSquares];
+int boardState[numSquares]; //boardstate as an array
 
 void setup() {
   // put your setup code here, to run once:
-  Serial.begin(9600);
+  Serial.begin(9600); //start serial
 
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-  for(int squareIndex = 0; squareIndex < numSquares; squareIndex++) {
-    int inputVoltage = analogRead(pins[squareIndex]);
-    int correspondingBuilding = -1;
-    for(int buildingIndex = 0; buildingIndex < numBuildings; buildingIndex++) {
+  for(int squareIndex = 0; squareIndex < numSquares; squareIndex++) { //for each square in the array
+    int inputVoltage = analogRead(pins[squareIndex]); //find voltage output from that square
+    //find corresponding building to output voltage
+    int correspondingBuilding = -1; //-1 if no associated building
+    for(int buildingIndex = 0; buildingIndex < numBuildings; buildingIndex++) { //for each building voltage
       if (abs(buildingVoltages[buildingIndex] - inputVoltage) < tolerance) {
-        correspondingBuilding = buildingIndex;
+        correspondingBuilding = buildingIndex; //assigns building
         break;
       }
     }
-    boardState[squareIndex] = correspondingBuilding;
+    boardState[squareIndex] = correspondingBuilding; //current square on the board is assigned the found building
   }
+  //prints array to serial
   Serial.print("ArrayState:");
   for(int i = 0; i < boardState; i++){
     Serial.print(" ");
