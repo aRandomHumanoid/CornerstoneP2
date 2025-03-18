f3d66150229151fcf5b76ff3ee78043f45dd49ef
avats123 <anshvats12345@gmail.com>
2025-03-18 15:53:16 -0400

    added changes

--- Code/P2ProofOfConcept/P2ProofOfConcept.ino
+++ Code/P2ProofOfConcept/P2ProofOfConcept.ino
@@ -1,38 +1,40 @@
-const int numSquares = 4; //total num of squares;
-const int pins[] = {1, 2, 3, 4}; 
+const int pinArray[] = {A0, A1, A2, A3}; //storing the four analog pins in array so we can use for loop to iterate over them - analog to read diff voltages
+const int tolerance = 10; //how much we allow voltage to vary for building, small number so voltages of different buildings dont overlap
+const int num_squares = 4; // number of building squares
+const int num_buildings = 4; // number of buildings
 
-const int numBuildings = 5;
-const int buildingVoltages[] = {10, 20, 30, 45, 50};
+int boardState[num_squares]; //empty array
+const int buildingVoltages[] = {847, 893, 932, 1001}; // array of 
 
-const int tolerance = 5; //tolerance for voltage
-
-int boardState[numSquares]; //boardstate as an array
 
 void setup() {
-  // put your setup code here, to run once:
-  Serial.begin(9600); //start serial
+  Serial.begin(9600);
 
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-  for(int squareIndex = 0; squareIndex < numSquares; squareIndex++) { //for each square in the array
-    int inputVoltage = analogRead(pins[squareIndex]); //find voltage output from that square
-    //find corresponding building to output voltage
+  for(int squareIndex = 0; squareIndex < num_squares; squareIndex++) { //for each square in the array
+    int inputVoltage = analogRead(pinArray[squareIndex]); // find voltage output from that square
+    // find corresponding building to output voltage
     int correspondingBuilding = -1; //-1 if no associated building
-    for(int buildingIndex = 0; buildingIndex < numBuildings; buildingIndex++) { //for each building voltage
+    for(int buildingIndex = 0; buildingIndex < num_buildings; buildingIndex++) { //for each building voltage, checking to see if voltage falls within tolerance
       if (abs(buildingVoltages[buildingIndex] - inputVoltage) < tolerance) {
-        correspondingBuilding = buildingIndex; //assigns building
-        break;
+        correspondingBuilding = buildingIndex; // assigns building value based on which building is within the tolerance, iterating over all the buildings
+        break; // break loop once all buildings are identified
       }
     }
     boardState[squareIndex] = correspondingBuilding; //current square on the board is assigned the found building
   }
   //prints array to serial
   Serial.print("ArrayState:");
-  for(int i = 0; i < boardState; i++){
+  for(int i = 0; i < num_squares; i++){
+    //Serial.print("You have just placed ");
     Serial.print(" ");
-    Serial.print(boardState[i]);
+    Serial.print(boardState[i]+1); // displays the numebr of the building placed,
+    //Serial.print(" on the board!");
   }
-  Serial.println();
+  Serial.println(); // prints to new line
 }
+
+
